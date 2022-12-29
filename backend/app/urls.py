e8d06082b9c02c1cac51a7ade20737fd14726289
brice franklin <bricefongang10@gmail.com>
2022-12-29 10:08:02 +0100

    Ecome first commit

---
+++ backend/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('', views.getRoutes, name='getRoutes'),    
+    path('products', views.getProducts, name='getProducts'),    
+    path('products/<pk>/', views.getProduct, name='getProduct'),    #(?P<pk>\d+)$
+]
